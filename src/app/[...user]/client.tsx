02aa3e21c1f4fe74aec1f200b6b41318247872b4
Andrew Jennings <73889377+thealmighyderpybird@users.noreply.github.com>
2025-11-14 12:36:39 -0500

    client button

--- src/app/[...user]/client.tsx
+++ src/app/[...user]/client.tsx
@@ -1,6 +1,8 @@
 import type { TreeInfo } from "~/lib/getTreeInfo";
 
 export default function Client({ sid, displayName, treeInfo }: { sid: string, displayName: string, treeInfo: TreeInfo }) {
-    return <button className="p-3 leading-none rounded-xl bg-orange-400 hover:opacity-90 active:opacity-70 w-fit min-w-32"
-                   >Add a message</button>
-}
+    return <div className="flex justify-center">
+        <button className="p-3 leading-none rounded-xl bg-orange-400 hover:opacity-90 active:opacity-70 w-fit min-w-32"
+               >Add a message</button>
+    </div>;
+};
